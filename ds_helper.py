6cb9f51bba7c0998246a3b45ce6a549ce5aa42cf
Shadi Massalha <shady.massalha@gmail.com>
2018-07-09 22:57:28 +0300

    modify create temp file method

--- ds_helper.py
+++ ds_helper.py
@@ -1,3 +1,9 @@
+import tempfile
+from pprint import pprint
+
+from ds_config import DSConfig
+
+
 class DSHelper:
     def __init__(self):
         pass
@@ -19,5 +25,9 @@ class DSHelper:
         pass
 
     @classmethod
-    def createPrivateKeyTempFile(cls, fileSuffix):
-        pass
+    def create_private_key_temp_file(cls, file_suffix):
+        tmp_file = tempfile.NamedTemporaryFile(mode='w+b', suffix=file_suffix)
+        f = open(tmp_file.name, "w+")
+        f.write(DSConfig.private_key())
+        f.close()
+        return tmp_file
