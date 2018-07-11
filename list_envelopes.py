079991596490171f3a8ae51f32a5365d2f87735c
Shadi Massalha <shady.massalha@gmail.com>
2018-07-11 17:54:31 +0300

    complete printing envelope list

--- list_envelopes.py
+++ list_envelopes.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from datetime import timedelta
 from docusign_esign import EnvelopesApi
 
 from example_base import ExampleBase
@@ -8,10 +10,6 @@ class ListEnvelopes(ExampleBase):
     def list(self):
         self.check_token()
 
-        envelopeApi = EnvelopesApi(ListEnvelopes.api_client);
-
-        options = {}
-        # $date = newDatetime();
-        # $date->sub(DateInterval("P30D"))
-        # options->setFromDate($date->format("Y/m/d"))
-        return envelopeApi.list_status_changes(ListEnvelopes.accountID, options)
+        envelope_api = EnvelopesApi(ListEnvelopes.api_client);
+        from_date = (datetime.now() + timedelta(days=-30)).strftime("%Y/%m/%d")
+        return envelope_api.list_status_changes(ListEnvelopes.accountID, from_date=from_date)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import docusign_esign as docusign
 
+from ds_helper import DSHelper
 from list_envelopes import ListEnvelopes
 from send_envelope import SendEnvelope
 
@@ -10,11 +11,19 @@ def main():
     print("\nSending an envelope...")
     result = SendEnvelope(api_client).send_envelope()
 
-    print("Envelope status: %s. Envelope ID: %s".format(result.getStatus(), result.getEnvelopeId()));
+    print("Envelope status: %s. Envelope ID: %s".format(result.status, result.envelope_id))
 
     print("\nList envelopes in the account...")
-    envelopesList = ListEnvelopes(api_client).list()
-    #TODO: print the first 2 envelopes like c#, java and php
+    envelopes_list = ListEnvelopes(api_client).list()
+    envelopes = envelopes_list.envelopes
+    if envelopes_list is not None and len(envelopes) > 2:
+        print("Results for %d envelopes were returned. Showing the first two:"
+              .format(len(envelopes_list.envelopes)))
+
+        envelopes_list.envelopes = [envelopes[0], envelopes[1]]
+
+    DSHelper.print_pretty_json(envelopes_list)
+
     print("\nDone.")
 
 
